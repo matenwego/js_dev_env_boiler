83291e44b75904d1d9c41d58f31afb9d0877ec08
Ricardo Aponte <raponte@gmail.com>
2017-10-11 08:22:55 -0400

    Updating with latest changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
 
-// This is the default index
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must user array.from to create a real array from a DOM collection
+  // getElementsByClassName on returns an "array like" object
+  Array.from(deleteLinks, link =>{
+    link.onclick = (event) => {
+      const element = event.target;
+
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
